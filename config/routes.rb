4196671331ef8938601dea2e0f0181e84b799928
cesarfuster <cfuster@uol.com.br>
2018-06-22 14:13:22 -0300

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,20 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'dashboards/index'
+Rails.application.routes.draw ​do
+  devise_for ​:users
+  namespace ​:api​ ​do
+    namespace ​:v1​ ​do
+      get ​'/dashboard'​, ​to:​ ​'dashboards#index'​, ​as:​ ​'dashboard'
+      resources ​:favorites​, ​path:​ ​"my_list"​, ​only:​ %i( index create ) delete ​'/my_list/:type/:id'​, ​to:​ ​'favorites#destroy'
+      resources ​:reviews​, ​only:​ [​:index​, ​:create​]
+      resources ​:searches​, ​path:​ ​"search"​, ​only:​ ​:index
+      resources ​:series​, ​only:​ ​:show
+      resources ​:movies​, ​only:​ ​:show​ ​do
+        member ​do
+          get ​'/executions'​, ​to:​ ​'executions#show'
+          put ​'/executions'​, ​to:​ ​'executions#update'
+        end
+      end
+      resources ​:recommendations​, ​only:​ ​:index
     end
   end
-  namespace :api do
-    namespace :v1 do
-      get 'recommendations/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'executions/create'
-      get 'executions/update'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'series/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'movies/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'searches/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'reviews/index'
-      get 'reviews/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'favorites/index'
-      get 'favorites/create'
-      get 'favorites/destroy'
-    end
-  end
-  devise_for :users
-  root to: "home#index"
+  root ​:to​ => ​"home#index"
 end
