eb413912329a9000f771bd9fa8adbbb8834abd9a
cesarfuster <cfuster@uol.com.br>
2018-06-22 14:19:50 -0300

    Favorites controller completed actions

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,10 +1,22 @@
 class Api::V1::FavoritesController < ApplicationController
+
   def index
+    @favorites = current_user.favorites.all
+    render ​json: Api::V1::WatchableSerializer.new(@favorites.map(&​:favoritable​)).serialized_json
   end
 
   def create
+    @favorite = Favorite.new(favorite_params)
+    if​ @favorite.save
+      head ​:ok
+    else
+      render ​json:​ { ​errors:​ @favorite.errors.full_messages }, ​status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @favorite.destroy
+    head ​:ok
   end
+
 end
