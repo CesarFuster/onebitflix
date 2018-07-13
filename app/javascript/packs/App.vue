0ddf7d6bc85c20c7ed0ad347dd16671729bec725
cesarfuster <cfuster@uol.com.br>
2018-07-13 14:28:00 -0300

    Config routes.vue

--- app/javascript/packs/App.vue
+++ app/javascript/packs/App.vue
@@ -1,22 +1,22 @@
 <template>
  <v-app>
-  <h4 class="white--text">Header Component</h4>
+  <Header/>
   <v-content>
-   <h4 class="white--text">Main Component</h4>
+   <router-view></router-view>
   </v-content>
   <Footer/>
  </v-app>
 </template>
 
 <script>
-  import Header from './components/shared/header.vue';
-  import Footer from './components/shared/footer.vue';
+ import Header from './components/shared/header.vue';
+ import Footer from './components/shared/footer.vue';
 
-  export default {
-   data () {
-    return {
-   }
-  },
+ export default {
+  data () {
+   return {
+  }
+ },
   components: {
    'Footer': Footer,
    'Header': Header
